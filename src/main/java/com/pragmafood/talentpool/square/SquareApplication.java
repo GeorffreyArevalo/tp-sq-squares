18c77f1ae078dca30aa5a4706948b459f38259c6
georffrey.arevalo <georffrey.arevalo@pragma.com.co>
2026-03-20 10:23:28 -0500

    Initial Configurations

---
+++ src/main/java/com/pragmafood/talentpool/square/SquareApplication.java
@@ -0,0 +1,13 @@
+package com.pragmafood.talentpool.square;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareApplication.class, args);
+	}
+
+}
